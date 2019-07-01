8122dc0cdfbe329805ea2fcdf7812b5c3ab9f829
Zech Drinkhall <zechdrink@aol.com>
2019-06-30 23:45:39 -0700

    fixed a couple of errors

--- api/server.js
+++ api/server.js
@@ -9,7 +9,8 @@ const authRouter = require('../helpers/00-auth/auth-router')
 const userRouter = require('../helpers/01-user/user-router');
 const workoutRouter = require('../helpers/02-workout/workout-router');
 const exerciseRouter = require('../helpers/03-exercise/exercise-router');
-const setRouter = require('../helpers/04-set/set-router');
+const targetRouter = require('../helpers/04-targetarea/target-router');
+const setRouter = require('../helpers/05-set/set-router');
 
 server.use(express.json());
 server.use(helmet());
@@ -19,6 +20,7 @@ server.use('/auth', authRouter);
 server.use('/users', authenticate, userRouter);
 server.use('/workouts', authenticate, workoutRouter);
 server.use('/exercises', authenticate, exerciseRouter);
+server.use('/targets', authenticate, targetRouter);
 server.use('/sets', authenticate, setRouter);
 
 server.get('/', (req, res) => {
